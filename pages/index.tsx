e5d1368e7e542d5a7ba384fda4cf6825ef535b9d
apothuoctober <pothuaud@october.eu>
2022-12-10 08:23:50 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,7 +96,7 @@ const Page$Home: NextPage = () => {
   }, [])
 
   const isConflictingOnTodoName = todoFormError !== null && todoFormError.includes('already exists')
-  const isConflicting = (todoName: string): boolean => {
+  const isConflictingTodo = (todoName: string): boolean => {
     return isConflictingOnTodoName && todoFormName === todoName
   }
 
@@ -141,7 +141,7 @@ const Page$Home: NextPage = () => {
         */}
         <ul className='todos-list'>
           {todosList.map((todo) => (
-            <li className={classNames('todos-list-item', { completed: todo.completed, conflict: isConflictingTodo(todo.name) })} key={todo.key}>
+            <li className={classNames('todos-list-item', { completed: todo.completed, conflict: isConflictingTodo("todo.name") })} key={todo.key}>
               <input type='checkbox' checked={todo.completed} onChange={() => { toggleTodoHandler(todo) }} />
               <p className={classNames({ completed: todo.completed })}>{todo.name}</p>
               <button className='delete-todo' type='button' onClick={() => { removeTodoHandler(todo) }}>❌</button>
